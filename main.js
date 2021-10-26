11645b093bc10251254ad87f11331d80e3a03016
j0tappe <jotappe-@hotmail.com>
2021-10-26 20:30:20 -0300

    https

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute("class");
 
-    li.children[0].href = `http://${social}.com/${LinksSocialMedia[social]}`;
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`;
   }
 }
 
 changeSocialMediaLinks();
 
 function getGitHubProfileInfos() {
-  const url = `http://api.github.com/users/${LinksSocialMedia.github}`;
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`;
 
   fetch(url)
     .then((response) => response.json())
